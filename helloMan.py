f1fc26ce44f026c6220fe7f9b953e9e2fa8dc4f5
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2018-12-24 00:26:29 +0300

    hello man in python3

---
+++ helloMan.py
@@ -0,0 +1,7 @@
+# Our program hello man in python3
+# Creatring an input in the helloMan program
+man = input('Enter the man you know: ')
+# The variable hello
+hello = "Hello"
+# Printing the hello man
+print("{0} {1} !! :-).".format(hello, man))
